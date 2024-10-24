74a0038dbfdc47528b83d7131c4b9b29ea33b69a
Musiker15 <info@musiker15.de>
2024-10-24 20:54:33 +0200

    Update functions.lua

--- shared/functions.lua
+++ shared/functions.lua
@@ -1,14 +1,16 @@
+MSK.Config = Config
+
 MSK.GetConfig = function()
-    return Config
+    return MSK.Config
 end
-exports('GetConfig', MSK.GetConfig)
 exports('Config', MSK.GetConfig)
+exports('GetConfig', MSK.GetConfig)
 
 MSK.Logging = function(code, ...)
     assert(code and type(code) == 'string', 'Parameter "code" has to be a string on function MSK.Logging')
-    print(('%s %s'):format(('[^2%s^0]'):format(GetInvokingResource() or 'msk_core'), Config.LoggingTypes[code] or Config.LoggingTypes['debug']), ..., '^0')
+    print(('[^2%s^0] %s'):format(GetInvokingResource() or 'msk_core', Config.LoggingTypes[code] or Config.LoggingTypes['debug']), ..., '^0')
 end
-MSK.logging = MSK.Logging -- Support for old Versions
+MSK.logging = MSK.Logging -- Backwards compatibility
 exports('Logging', MSK.Logging)
 
 logging = function(code, ...)
